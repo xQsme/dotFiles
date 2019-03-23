8a1ff32ba9ada8bf93eea0335ca71effd8559c2a
xQsme <2151163@my.ipleiria.pt>
2019-03-23 17:38:30 +0000

    laptop

--- laptop/rofi/scripts/powermenu.sh
+++ laptop/rofi/scripts/powermenu.sh
@@ -3,21 +3,22 @@
 rofi_command="rofi -columns 4 -lines 1 -theme themes/powermenu.rasi"
 
 # Each of the icon is a selectable element
-options=$'\n\n\n'
+options=$'\n\n\n'
 
 chosen="$(echo "$options" | $rofi_command -font "Font Awesome 5 Pro Solid 85" -dmenu)"
 case $chosen in
     ) # Lock the screen
-        dm-tool lock
+        scrot -q 100 /tmp/lock.png; mantablockscreen -i /tmp/lock.png; mantablockscreen -cc
         ;;
     ) # Shutdown the computer
         systemctl poweroff
         ;;
-    ) # Reboot the computer
+    ) # Reboot the computer
         systemctl reboot
         ;;
-    ) # Log out of the current session
+    ) # Log out of the current session
         i3-msg exit
         ;;
 esac
+
 
