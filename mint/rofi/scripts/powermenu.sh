0af91f742beb0b210cfd572e9de68464c63ea0ea
Pedro Ferreira <pedro.ferreira@voidsoftware.com>
2020-01-29 10:14:40 +0000

    mint

---
+++ mint/rofi/scripts/powermenu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rofi_command="rofi -columns 4 -lines 1 -theme themes/powermenu.rasi"
+
+# Each of the icon is a selectable element
+options='\n\n\n'
+
+chosen="$(echo "$options" | $rofi_command -font "FontAwesome 60" -dmenu)"
+case $chosen in
+    ) # Lock the screen
+        scrot -q 100 /tmp/lock.png; mantablockscreen -i /tmp/lock.png; mantablockscreen -cc
+        ;;
+    ) # Shutdown the computer
+        systemctl poweroff
+        ;;
+    ) # Reboot the computer
+        systemctl reboot
+        ;;
+    ) # Log out of the current session
+        i3-msg exit
+        ;;
+esac
+
